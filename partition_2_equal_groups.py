4e0ed7c697d4bf4085f3201fd5e7be3aab732c0f
Saravanan.M <46522992+saravananmahalingam@users.noreply.github.com>
2019-05-20 11:17:25 +0530

    Create partition_2_equal_groups.py

---
+++ partition_2_equal_groups.py
@@ -0,0 +1,10 @@
+# sum of last 2 values equal times and get first value
+N,A,B=map(int,input().split())
+flag=0
+for n in range(0,N):
+	if(n*A + n*B)==N:
+		flag=1
+if flag==1:
+	print("yes")
+else:
+	print("no")
